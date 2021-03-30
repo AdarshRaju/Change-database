7fa9c8d5e1b8bff52bdfd702fc9238de46645f50
AdarshRaju <66949659+AdarshRaju@users.noreply.github.com>
2021-03-30 21:07:18 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-import { testPurses } from "./shortChangeTests.js"
+function roll(min, max, floatFlag) {
+    let r = Math.random() * (max - min) + min
+    return floatFlag ? r : Math.floor(r)
+}
+
+let testPurses = Array(5).fill(0).map(a => {
+    return {
+        quarters: roll(0, 15),
+        dimes: roll(0, 30),
+        nickels: roll(0, 40),
+        pennies: roll(0, 50),
+        price: Number(roll(0, 100, 1).toFixed(2))
+    }
+})
 
 const purchaseConfirmation = document.getElementById("purchase-confirmation")
 const quarterCounter = document.getElementById("quarter-count")
