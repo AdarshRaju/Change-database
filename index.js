0add3ce0339bd77131e48f5146910b7db74d3d88
AdarshRaju <66949659+AdarshRaju@users.noreply.github.com>
2021-03-30 20:57:20 +1100

    Create index.js

---
+++ index.js
@@ -0,0 +1,62 @@
+import { testPurses } from "./shortChangeTests.js"
+
+const purchaseConfirmation = document.getElementById("purchase-confirmation")
+const quarterCounter = document.getElementById("quarter-count")
+const dimeCounter = document.getElementById("dime-count")
+const nickelCounter = document.getElementById("nickel-count")
+const pennyCounter = document.getElementById("penny-count")
+
+var arr = []
+
+let counter = 0;
+
+const prev = document.getElementById('previous-case')
+const next = document.getElementById('next-case')
+
+if (counter === 0){
+    prev.disabled = true;
+} else {prev.disabled = false;}
+
+function update() {
+    quarterCounter.innerHTML = testPurses[counter].quarters;
+    dimeCounter.innerHTML = testPurses[counter].dimes;
+    nickelCounter.innerHTML = testPurses[counter].nickels;
+    pennyCounter.innerHTML = testPurses[counter].pennies;
+    const total = testPurses[counter].quarters + testPurses[counter].dimes + testPurses[counter].nickels + testPurses[counter].pennies;
+    console.log(total, testPurses[counter].price)
+    if (testPurses[counter].price > total) {
+        purchaseConfirmation.innerHTML = `With $${total} you cannot afford this $${testPurses[counter].price} purchase`;
+        purchaseConfirmation.style.background = 'red';
+    } else if (testPurses[counter].price <= total) {
+        purchaseConfirmation.innerHTML = `With $${total} you can afford this $${testPurses[counter].price} purchase`;
+        purchaseConfirmation.style.background = 'green';
+    }
+}
+update()
+prev.addEventListener('click', function(e) {
+    if (counter>0) {
+        if (counter === 0){
+    prev.disabled = true; }
+    else {
+        prev.disabled = false;
+        counter = counter - 1
+        update()
+        if (counter === 0){
+    prev.disabled = true; }
+        }
+        
+} if (counter === testPurses.length){   
+    next.disabled = true;}
+    else {next.disabled = false;}
+})
+
+next.addEventListener('click', function(e) {
+    
+   if (counter < testPurses.length) { 
+    prev.disabled = false;   
+    counter = counter + 1;
+    if (counter === testPurses.length - 1){
+    next.disabled = true;}
+    update()
+    }   
+})
